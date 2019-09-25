cfba8e96fc643e0c866b849385b910fb0af9c02d
mybene <benoite251@gmial.com>
2019-09-25 15:18:53 +0200

    remove pipes and directives

--- src/app/highligthed.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HighligthedDirective } from './highligthed.directive';
-
-describe('HighligthedDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HighligthedDirective();
-    expect(directive).toBeTruthy();
-  });
-});
